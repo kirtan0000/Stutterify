b1c434d27f12b3b5f83cb735fc5451a514e2c1c4
kirtan <kirtanmagan@gmail.com>
2021-01-24 09:28:50 -0800

    Added many features!

---
+++ index.js
@@ -0,0 +1,56 @@
+module.exports = function stutterify(string, prob_stutter = 0.7, prob_length = 0.37) {
+    var sent = "";
+    var str = "";
+
+    const stutter_word = (word, count) => {
+        const stutter_length = Math.max(1, word.match(/[aiueo]/i).index);
+        const stutter_part = word.substr(0, stutter_length);
+        var result = "";
+
+        for (var i = 0; i < count; i++) 
+            result += stutter_part + "-";
+        result += word;
+
+        return result;
+    }
+
+    string.split(" ").forEach(item => {
+
+    if (item.match(/[a-z]/i)) {
+        if (Math.random() < prob_stutter) {
+
+            if (Math.random() < prob_length) {
+
+                str = stutter_word(item, 2) + " ";
+
+            } else {
+
+                str = stutter_word(item, 1) + " ";
+
+            }
+            sent += str;
+
+        } else {
+
+            if (item != string.split(" ")[string.length]) {
+
+                str = item + " ";
+
+            } else {
+
+                str = item;
+
+            }
+            sent += str;
+
+        }
+
+    } else {
+
+        sent += item;
+
+    }
+
+    })
+    return sent.replace(/\s*$/,"");;
+};
