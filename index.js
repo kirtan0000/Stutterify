bd19ebb9d30d1418fe9314c26f2ce20d93d750a3
kirtan <kirtanmagan@gmail.com>
2021-01-24 10:42:17 -0800

    Fixed index bug.

--- index.js
+++ index.js
@@ -3,12 +3,30 @@ module.exports = function stutterify(string, prob_stutter = 0.7, prob_length = 0
     var str = "";
 
     const stutter_word = (word, count) => {
-        const stutter_length = Math.max(1, word.match(/[aiueo]/i).index);
-        const stutter_part = word.substr(0, stutter_length);
-        var result = "";
+        const stutter_str = Math.max(1, word.match(/[aiueo]/i));
+        if (stutter_str.index) {
+            const stutter_length = stutter_str.index;
+            const stutter_part = word.substr(0, stutter_length);
+            var result = "";
 
-        for (var i = 0; i < count; i++) 
-            result += stutter_part + "-";
+            for (var i = 0; i < count; i++) {
+                result += stutter_part + "-";
+
+            }
+
+        } else {
+
+            const stutter_part = word.substr(0, 1);
+            var result = "";
+
+            for (var i = 0; i < count; i++) {
+                result += stutter_part + "-";
+
+            }
+
+
+        }
+
         result += word;
 
         return result;
@@ -16,41 +34,42 @@ module.exports = function stutterify(string, prob_stutter = 0.7, prob_length = 0
 
     string.split(" ").forEach(item => {
 
-    if (item.match(/[a-z]/i)) {
-        if (Math.random() < prob_stutter) {
+        if (item.match(/[a-z]/i)) {
+            if (Math.random() < prob_stutter) {
 
-            if (Math.random() < prob_length) {
+                if (Math.random() < prob_length) {
 
-                str = stutter_word(item, 2) + " ";
+                    str = stutter_word(item, 2) + " ";
+
+                } else {
+
+                    str = stutter_word(item, 1) + " ";
+
+                }
+                sent += str;
 
             } else {
 
-                str = stutter_word(item, 1) + " ";
+                if (item != string.split(" ")[string.length]) {
+
+                    str = item + " ";
+
+                } else {
+
+                    str = item;
+
+                }
+                sent += str;
 
             }
-            sent += str;
 
         } else {
 
-            if (item != string.split(" ")[string.length]) {
-
-                str = item + " ";
-
-            } else {
-
-                str = item;
-
-            }
-            sent += str;
+            sent += item;
 
         }
 
-    } else {
-
-        sent += item;
-
-    }
-
     })
-    return sent.replace(/\s*$/,"");;
+
+    return sent.replace(/\s*$/, "");;
 };
