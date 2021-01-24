63b3526953ccb628cb88f1b8b069346ca4080048
Damiann Mangan <damiann.mangan@finaccel.co>
2021-01-24 14:37:30 +0700

    remove to-upper-case on stutter

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
-module.exports = function stutterify(string) {
+module.exports = function stutterify(string, prob_stutter = 0.7, prob_length = 0.37) {
     var sent = "";
     var str = "";
 
+    const stutter_initial = (word, count) => {
+        var first_char = word.substr(0, 1);
+        var stutter_char = "";
+
+        for (var i = 0; i < count; i++) 
+            stutter_char += first_char + "-";
+
+        return stutter_char + word;
+    }
+
     string.split(" ").forEach(item => {
 
-    if(item.match(/[a-z]/i)) {
-        if (Math.random() < 0.7) {
+    if (item.match(/[a-z]/i)) {
+        if (Math.random() < prob_stutter) {
 
-            if (Math.random() < 0.37) {
+            if (Math.random() < prob_length) {
 
-                str = item.substr(0, 1).toUpperCase() + "-" + item.substr(0, 1).toUpperCase() + "-" + item.substr(0, 1).toUpperCase() + item.substr(1, item.length) + " ";
+                str = stutter_initial(item, 2) + " ";
 
             } else {
-                str = item.substr(0, 1).toUpperCase() + "-" + item.substr(0, 1).toUpperCase() + item.substr(1, item.length) + " ";
+
+                str = stutter_initial(item, 1) + " ";
 
             }
             sent += str;
@@ -27,10 +38,8 @@ module.exports = function stutterify(string) {
 
                 str = item;
 
-
             }
             sent += str;
-
 
         }
 
