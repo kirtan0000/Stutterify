75c14d9ab1b9b8926a2534a6c8ffc5c621e152ed
kirtan <kirtanmagan@gmail.com>
2021-01-27 16:26:26 -0800

    Added compacted code

--- index.js
+++ index.js
@@ -8,68 +8,40 @@ module.exports = function stutterify(string, prob_stutter = 0.7, prob_length = 0
             const stutter_length = stutter_str.index;
             const stutter_part = word.substr(0, stutter_length);
             var result = "";
-
             for (var i = 0; i < count; i++) {
                 result += stutter_part + "-";
-
             }
-
         } else {
-
-            const stutter_part = word.substr(0, 1);
+            const stutter_part = word.substr(0, 2);
             var result = "";
-
             for (var i = 0; i < count; i++) {
                 result += stutter_part + "-";
-
             }
-
 
         }
-
         result += word;
-
         return result;
     }
 
-    string.split(" ").forEach(item => {
-
-        if (item.match(/[a-z]/i)) {
-            if (Math.random() < prob_stutter) {
-
-                if (Math.random() < prob_length) {
-
-                    str = stutter_word(item, 2) + " ";
-
+    string.split(" ")
+        .forEach(item => {
+            if (item.match(/[a-z]/i)) {
+                if (Math.random() < prob_stutter) {
+                    if (Math.random() < prob_length) str = stutter_word(item, 2) + " ";
+                    else str = stutter_word(item, 1) + " ";
+                    sent += str;
                 } else {
-
-                    str = stutter_word(item, 1) + " ";
-
+                    if (item != string.split(" ")[string.length]) {
+                        str = item + " ";
+                    } else {
+                        str = item;
+                    }
+                    sent += str;
                 }
-                sent += str;
-
             } else {
-
-                if (item != string.split(" ")[string.length]) {
-
-                    str = item + " ";
-
-                } else {
-
-                    str = item;
-
-                }
-                sent += str;
-
+                sent += item;
             }
-
-        } else {
-
-            sent += item;
-
-        }
-
-    })
+        })
 
     return sent.replace(/\s*$/, "");;
 };
